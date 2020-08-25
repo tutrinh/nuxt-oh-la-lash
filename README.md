ac5e88b9a1083737c91a84daea81c2cb29b91b4a
Tu Trinh <tu.trinh@heart.org>
2020-08-25 14:57:10 -0500

    Custom variables for SASS

--- README.md
+++ README.md
@@ -18,3 +18,26 @@ $ npm run generate
 ```
 
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+
+## Deploy
+https://app.netlify.com/teams/tutrinh/sites
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/45a30f24-79c7-4d6b-a666-7bdd74d18d5f/deploy-status)](https://app.netlify.com/sites/laughing-carson-39350b/deploys)
+
+## Fonts
+<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Alata&family=Cutive+Mono&family=Josefin+Sans:wght@300;400;500&family=Permanent+Marker&family=Rozha+One&display=swap" rel="stylesheet">
+
+```css
+font-family: 'Abril Fatface', cursive;
+font-family: 'Alata', sans-serif;
+font-family: 'Cutive Mono', monospace;
+font-family: 'Josefin Sans', sans-serif;
+font-family: 'Permanent Marker', cursive;
+font-family: 'Rozha One', serif;
+```
+
+## Colors
+https://colorhunt.co/palette/171239
+https://colorhunt.co/palette/183392
+https://colorhunt.co/palette/114174
+
--- pages/fun.vue
+++ pages/fun.vue
@@ -6,7 +6,46 @@
 
 <script>
     export default {
-        
+        name: 'Fun',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            async fetchData() {
+                try {
+                    const response = await fetch('http://www.mocky.io/v2/5cf0317e30000053da3cd54a?mocky-delay=2000ms');
+                    const data = await response.json();
+                    console.log(data);
+                    
+                    
+                    
+                } catch(errors) {
+                    console.log(errors);
+                } finally {
+                    console.log("done");
+                    
+                }
+  
+  
+            }
+
+
+        },
+        created() {
+            this.fetchData();
+
+
+            const fruits = ['apples', 'grapes', 'bananas'];
+            console.log(fruits);
+            console.log(fruits.join(','));
+            const fruitAlt = [...fruits];
+            fruits.push('oranges');
+            console.log(fruitAlt);
+            console.log(fruits);
+        }
+
     }
 </script>
 
